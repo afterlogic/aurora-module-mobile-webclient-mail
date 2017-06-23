29f92cd49631684782c74a63b03afc859adff58c
nadine-shi <nadine@afterlogic.com>
2017-06-23 14:37:56 +0300

    Add files via upload

---
+++ js/manager.js
@@ -0,0 +1,67 @@
+'use strict';
+
+module.exports = function (oAppData) {
+	require('modules/MailWebclient/js/enums.js');
+	
+	var
+		_ = require('underscore'),
+		ko = require('knockout'),
+		
+		App = require('%PathToCoreWebclientModule%/js/App.js'),
+		MailSettings = require('modules/MailWebclient/js/Settings.js')
+	;
+	
+	if (App.getUserRole() === Enums.UserRole.NormalUser)
+	{
+		if (App.isMobile())
+		{
+			var Cache = require('modules/MailWebclient/js/Cache.js');
+			Cache.init();
+			var AccountList = require('modules/MailWebclient/js/AccountList.js');
+
+			return {
+				enableModule: ko.observable(MailSettings.AllowAddAccounts || AccountList.hasAccount() ),
+				start: function (ModulesManager) {
+					var
+						Browser = require('%PathToCoreWebclientModule%/js/Browser.js'),
+						MailUtils = require('modules/MailWebclient/js/utils/Mail.js')
+					;
+
+					require('modules/MailWebclient/js/koBindings.js');
+
+					if (MailSettings.AllowAppRegisterMailto)
+					{
+						MailUtils.registerMailto(Browser.firefox);
+					}
+
+					ko.computed(function () {
+						var aAuthAcconts = _.filter(AccountList.collection(), function (oAccount) {
+							return oAccount.useToAuthorize();
+						});
+
+						MailSettings.userAccountsCount(aAuthAcconts.length);
+					}, this);
+				},
+				getScreens: function () {
+					var oScreens = {};
+					oScreens[MailSettings.HashModuleName] = function () {
+						return require('modules/MailWebclient/js/views/MailView.js');
+					};
+					oScreens[MailSettings.HashModuleName + '-compose'] = function () {
+						var CComposeView = require('modules/MailWebclient/js/views/CComposeView.js');
+						return new CComposeView();
+					};
+					return oScreens;
+				},
+				getHeaderItem: function () {
+					return {
+						item: require('modules/%ModuleName%/js/views/HeaderItemView.js'),
+						name: MailSettings.HashModuleName
+					};
+				}
+			};
+		}
+	}
+	
+	return null;
+};
